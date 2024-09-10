6be9d3349e03f7277ed415023cdcd5807bf9fb1f
tahsin <u1704102@student.cuet.ac.bd>
2024-09-11 01:39:22 +0600

    nodeJS streams

---
+++ writeMany.mjs
@@ -0,0 +1,26 @@
+import * as fs from 'node:fs/promises';
+
+(async () => {
+    console.time("writeMany");
+    const fileHandle = await fs.open("output.txt","w");
+
+    const stream  = fileHandle.createWriteStream();
+
+    for(let i=0;i<1e6;i++) {
+        const buff = Buffer.from(` ${i}`,"utf-8");
+        stream.write(buff);
+    }
+    console.timeEnd("writeMany");
+}) ();
+
+// (async () => {
+//     console.time("writeMany");
+
+//     fs.open("output.txt","w", (err, fd)=> {
+//         for(let i=0;i<1e6;i++) {
+//             const buff = Buffer.from(` ${i}`,"utf-8");
+//             fs.writeSync(fd, buff);
+//         }
+//     })
+//     console.timeEnd("writeMany");
+// }) ();
