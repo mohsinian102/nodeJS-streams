e0204460de1df70f02ee085da1e9efd5b45914b3
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-09-11 10:03:46 +0600

    timeEnd function was at the wrong place

--- writeMany.mjs
+++ writeMany.mjs
@@ -1,26 +1,29 @@
-import * as fs from 'node:fs/promises';
-
-(async () => {
-    console.time("writeMany");
-    const fileHandle = await fs.open("output.txt","w");
-
-    const stream  = fileHandle.createWriteStream();
-
-    for(let i=0;i<1e6;i++) {
-        const buff = Buffer.from(` ${i}`,"utf-8");
-        stream.write(buff);
-    }
-    console.timeEnd("writeMany");
-}) ();
+import * as fs from 'node:fs';
 
 // (async () => {
 //     console.time("writeMany");
+//     const fileHandle = await fs.open("output.txt","w");
 
-//     fs.open("output.txt","w", (err, fd)=> {
-//         for(let i=0;i<1e6;i++) {
-//             const buff = Buffer.from(` ${i}`,"utf-8");
-//             fs.writeSync(fd, buff);
-//         }
-//     })
-//     console.timeEnd("writeMany");
+//     const stream  = fileHandle.createWriteStream();
+
+//     for(let i=0;i<1e6;i++) {
+//         const buff = Buffer.from(` ${i}`,"utf-8");
+//         stream.write(buff);
+//     }
+//     stream.end(() => {
+//         console.timeEnd("writeMany");
+//     });
 // }) ();
+
+(async () => {
+    console.time("writeMany");
+
+    fs.open("output.txt","w", (err, fd)=> {
+        for(let i=0;i<1e6;i++) {
+            const buff = Buffer.from(` ${i}`,"utf-8");
+            fs.writeSync(fd, buff);
+        }
+        console.timeEnd("writeMany");
+    })
+
+}) ();
