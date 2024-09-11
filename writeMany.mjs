8c3f18b70e150a925f1b55f9fdb412067c686889
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-09-11 16:03:53 +0600

    stream is more polished now

--- writeMany.mjs
+++ writeMany.mjs
@@ -1,29 +1,29 @@
-import * as fs from 'node:fs';
-
-// (async () => {
-//     console.time("writeMany");
-//     const fileHandle = await fs.open("output.txt","w");
-
-//     const stream  = fileHandle.createWriteStream();
-
-//     for(let i=0;i<1e6;i++) {
-//         const buff = Buffer.from(` ${i}`,"utf-8");
-//         stream.write(buff);
-//     }
-//     stream.end(() => {
-//         console.timeEnd("writeMany");
-//     });
-// }) ();
+import * as fs from 'node:fs/promises';
 
 (async () => {
     console.time("writeMany");
+    const fileHandle = await fs.open("output.txt","w");
 
-    fs.open("output.txt","w", (err, fd)=> {
-        for(let i=0;i<1e6;i++) {
-            const buff = Buffer.from(` ${i}`,"utf-8");
-            fs.writeSync(fd, buff);
+    const stream  = fileHandle.createWriteStream();
+    let i = 0;
+    const writeMany = () => {
+        while(i < 1e6) {
+            const buff = Buffer.from(` ${i}`, "utf-8");
+            if(i==999999) return stream.end(buff);
+            if(!stream.write(buff)) break;
+            i++;
         }
-        console.timeEnd("writeMany");
+    }
+
+
+    stream.on('drain',()=> {
+        writeMany();
     })
 
+    writeMany();
+
+    stream.on('finish', ()=> {
+        console.timeEnd("writeMany");
+        fileHandle.close();
+    })
 }) ();
